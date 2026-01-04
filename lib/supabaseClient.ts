750293f797d02fd54f24785c5fee2c56153dde8a
mecyahh <mecyah@opfgrp.com>
2026-01-04 11:16:34 -0500

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,9 +1,7 @@
+// lib/supabaseClient.ts
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
 export const supabase = createClient(
-  supabaseUrl,
-  supabaseAnonKey
+  process.env.NEXT_PUBLIC_SUPABASE_URL https://jvocbzliwbkndfpkbfdp.supabase.co,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp2b2Niemxpd2JrbmRmcGtiZmRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc0NzQwNzMsImV4cCI6MjA4MzA1MDA3M30.164fDmLp1qDWKPUSbdGoQPl945IG9i8jwZxInhQat5Y
 );
