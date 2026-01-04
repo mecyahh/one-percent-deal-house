d3141856be24845943277725203b8a7e82299c1c
mecyahh <mecyah@opfgrp.com>
2026-01-04 11:22:28 -0500

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,7 +1,7 @@
-// lib/supabaseClient.ts
+// lib/supabaseClient.ts  — REPLACE ENTIRE FILE
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL https://jvocbzliwbkndfpkbfdp.supabase.co,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp2b2Niemxpd2JrbmRmcGtiZmRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc0NzQwNzMsImV4cCI6MjA4MzA1MDA3M30.164fDmLp1qDWKPUSbdGoQPl945IG9i8jwZxInhQat5Y
+  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
 );
